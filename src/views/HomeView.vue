1c0028bfa74faaa9db95dd79f911552fe0346172
SGPyaePhyoThant <softguide.pyaephyothant@gmail.com>
2024-02-27 13:46:00 +0900

    vuetifyFirstDayRevision

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,52 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div class="home text-center">
+    <h3>諦めないで準備して続けること大切</h3>
+    <div class="purple darken-2 white--text text-center">
+      横浜市（よこはまし）は、神奈川県東部に位置する市。
+      神奈川県の県庁所在地及び人口が最多の市で、政令指定都市である。
+      日本有数の港湾都市・商工業都市でもある。
+    </div>
+    <br><br>
+
+    <v-btn
+  color="primary"
+  elevation="24"
+>横浜へ</v-btn>
+ <br><br>
+<v-btn
+  color="primary"
+  elevation="24"
+  plain
+>横浜へ</v-btn>
+ <br><br>
+ <v-btn
+  color="primary"
+  elevation="24"
+  text
+  top
+>横浜へ</v-btn>
+<br><br>
+<v-btn  class="pink white--text">
+ <v-icon left>mdi-email </v-icon>メール送る
+</v-btn>
+<br><br>
+<v-btn small  class="pink white--text" elevation="24">
+ <v-icon left>mdi mdi-airplane</v-icon>飛行機で日本へ行く
+</v-btn>
+<br><br>
+<v-btn depressed  class="primary" fab elevation="24">
+ <v-icon>mdi mdi-airplane</v-icon>
+</v-btn>
+    
+</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  export default {
+    name: 'Home',
+
+    components: {
+    },
   }
-}
 </script>
